e8e85146558949db7cc454f2f142d74a59aca488
Adrian White <snarkyboojum@gmail.com>
2020-03-15 15:36:00 +1100

    Added simple ppm generation

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,33 @@
 // 255 0 0    0 255 0  0 0 255
 // 255 255 0  255 255  255 0 0 0
 // 
-fn write_ppm() {
+fn write_ppm(w: i32, h: i32, max_value: i32) {
+    println!("P3\n{} {}\n{}", w, h, max_value);
+
+    for j in (0..h).rev() {
+        for i in 0..w {
+            let r = i as f32 / w as f32;
+            let g = j as f32 / h as f32;
+            let b: f32 = 0.2;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+
 
 }
 
 fn main() {
-    println!("A raytracer in Rust!");
+    //println!("A raytracer in Rust!");
+
+    let width = 200;
+    let height = 100;
+    let max_value = 255;
 
     // we use a plan txt ppm to start building images
-    write_ppm();
+    write_ppm(width, height, max_value);
 }
