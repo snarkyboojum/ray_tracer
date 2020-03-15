da7f4e65814d68edc218009c449cc47e52243293
Adrian White <snarkyboojum@gmail.com>
2020-03-15 15:07:19 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Overview
+
+A simple ray tracer written in Rust. Translated from [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) into Rust (from C++).
+
+## More information
+
+- [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// A simple ray tracer built by translating the tutorial
+// https://raytracing.github.io/books/RayTracingInOneWeekend.html
+// to Rust
+
+
+// Construct a ppm file for image data, e.g.
+// P3
+// 3 2
+// 255
+// 255 0 0    0 255 0  0 0 255
+// 255 255 0  255 255  255 0 0 0
+// 
+fn write_ppm() {
+
+}
+
+fn main() {
+    println!("A raytracer in Rust!");
+
+    // we use a plan txt ppm to start building images
+    write_ppm();
+}
